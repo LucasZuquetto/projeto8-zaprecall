c508e74f2a8e1e879d1983e08c29a9e931bf6a32
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-07-25 14:08:05 -0300

    Transição tela 1 para tela 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import Home from "./Home/Home"
 import InGameScreen from './InGameScreen/InGameScreen'
 import Questions from './Questions/Questions'
+import React from "react";
 
 export default function App(){
+    const [startApp, setstartApp] = React.useState(true);
+
     return(
         <>
-            <InGameScreen />
-            <Questions />
+            {startApp ? (
+                <Home startApp={startApp} setstartApp={setstartApp} />
+            ) : (
+                <>
+                    <InGameScreen />
+                    <Questions />
+                </>
+            )}
         </>
     )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,12 @@
 import "./style.css";
 import logo from '../../assets/logo.png'
 
-export default function Home () {
+export default function Home ({startApp, setstartApp}) {
     return(
         <div className="home">
             <img className="logo" src={logo} alt=""/>
             <h1>ZapRecall</h1>
-            <div className='button'>Iniciar Recall!</div>
+            <div className='button' onClick={() => setstartApp(!startApp)}>Iniciar Recall!</div>
         </div>
     )
 }
